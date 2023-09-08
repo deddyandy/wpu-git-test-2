63271a012913a639768db406be35439ca80910d7
Deddy Andy <dedyandy@gmail.com>
2023-09-08 09:47:27 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quasi sint, eum et veritatis pariatur molestias illum corporis dolore, cum assumenda voluptas, itaque atque? Iste eos vitae distinctio reprehenderit ducimus.</p>
 </body>
 </html>
